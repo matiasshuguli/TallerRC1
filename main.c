774939161f5757fea4745775f8b2ce1ee37f172b
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-30 19:00:52 -0500

    Commit 1 - Matias Shuguli: crear estructura inicial del programa

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int estudiantes;
+
+    printf("SISTEMA DE GESTION DE CALIFICACIONES\n");
+    printf("------------------------------------\n");
+
+    printf("Ingrese el numero de estudiantes: ");
+    scanf("%d", &estudiantes);
+
+    return 0;
+}
