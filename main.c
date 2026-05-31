ac443143b4ddfa647fddbcb2d8c1f9153c6abc46
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-30 19:41:04 -0500

    Commit 2 - Matias Shuguli: agregar ingreso y validacion de calificaciones

--- main.c
+++ main.c
@@ -2,21 +2,36 @@
 
 int main() {
     int estudiantes;
+    int i, j;
 
     printf("SISTEMA DE GESTION DE CALIFICACIONES\n");
     printf("------------------------------------\n");
 
     printf("Ingrese el numero de estudiantes: ");
     scanf("%d", &estudiantes);
-      while (estudiantes <= 0) {
 
-         printf("Error. Ingrese un numero de estudiantes mayor a 0: ");
+    while (estudiantes <= 0) {
+        printf("Error. Ingrese un numero de estudiantes mayor a 0: ");
+        scanf("%d", &estudiantes);
+    }
 
-         scanf("%d", &estudiantes);
-
-     }
- 
     float notas[estudiantes][3];
+
+    for (i = 0; i < estudiantes; i++) {
+        printf("\nEstudiante %d\n", i + 1);
+
+        for (j = 0; j < 3; j++) {
+            do {
+                printf("Ingrese la nota de la asignatura %d: ", j + 1);
+                scanf("%f", &notas[i][j]);
+
+                if (notas[i][j] < 0 || notas[i][j] > 10) {
+                    printf("Error: la nota debe estar entre 0 y 10.\n");
+                }
+
+            } while (notas[i][j] < 0 || notas[i][j] > 10);
+        }
+    }
 
     return 0;
 }
